5a416ceac5939b9d50ed82fe88abff2cd532a0fe
DrewnRose0133 <deviouska24de@gmail.com>
2025-04-21 17:31:17 -0400

    Delete JarvisAI.Features directory

--- JarvisAI.Features/ApprovedUsers.cs
+++
@@ -1,8 +0,0 @@
-
-// Static approved user store
-public static class ApprovedUsers
-{
-    public static HashSet<string> Users = new HashSet<string> { "tony", "pepper", "james" };
-
-    public static bool IsApproved(string user) => Users.Contains(user);
-}
