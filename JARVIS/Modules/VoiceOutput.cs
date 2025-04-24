4837abf993a80877c039b609b742c3d31153e9ce
DrewnRose0133 <deviouska24de@gmail.com>
2025-04-24 10:29:11 -0400

    Update VoiceOutput.cs

--- JARVIS/Modules/VoiceOutput.cs
+++ JARVIS/Modules/VoiceOutput.cs
@@ -13,25 +13,30 @@ namespace JARVIS.Modules
 
         static VoiceOutput()
         {
-            Synth.SelectVoice("Microsoft David Desktop");
+            Synth.SelectVoice("Microsoft Hazel Desktop");
             Synth.Rate = 0;
             Synth.Volume = 100;
         }
 
         public static Task SpeakAsync(string text)
         {
-            // wrap your text in SSML and escape it
             string ssml = $@"
-             <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-GB'>
-               <voice name='{Synth.Voice.Name}'>
-                 <prosody rate='-10%' pitch='-6st'>
-                   {System.Security.SecurityElement.Escape(text)}
-                 </prosody>
-               </voice>
-             </speak>";
-            
-                 // SpeakSsml is synchronous, so run it on a thread-pool thread
-                 return Task.Run(() => Synth.SpeakSsml(ssml));
+                <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-GB'>
+                  <voice name='Microsoft Hazel Desktop'>
+                    <!-- slow way down and drop the pitch way down -->
+                    <prosody rate='-20%' pitch='-50st'>
+                      <!-- add a pause before starting for dramatic effect -->
+                      <break time='300ms'/>
+                      {System.Security.SecurityElement.Escape(text)}
+                      <!-- add a little trailing pause -->
+                      <break time='200ms'/>
+<emphasis level=""moderate"">Good morning, sir.</emphasis>
+                    </prosody>
+                  </voice>
+                </speak>";
+
+            // SpeakSsml is synchronous, so run it on a thread-pool thread
+            return Task.Run(() => Synth.SpeakSsml(ssml));
         }
     }
 }
